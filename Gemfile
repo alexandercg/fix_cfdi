d9c6cb56189730f52bbccc988c442f0e78a569db
Alexander Camacho Gámez <alexandercg7@gmail.com>
2016-05-27 09:45:45 -0700

    Added gem dependencies and method to validates RFCs

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in fix_cfdi.gemspec
 gemspec
+
+gem 'nokogiri', '~> 1.6', '>= 1.6.7.2'
+gem 'mechanize', '~> 2.7', '>= 2.7.4'
--- lib/fix_cfdi.rb
+++ lib/fix_cfdi.rb
@@ -1,5 +1,18 @@
+require 'nokogiri'
+require 'mechanize'
 require "fix_cfdi/version"
 
 module FixCfdi
   # Your code goes here...
+  def verify_rfc(rfc)
+    agent = Mechanize.new
+    rfc_page = agent.get 'https://portalsat.plataforma.sat.gob.mx/ConsultaRFC/'
+    check_form = rfc_page.form
+    rfc_field = check_form.field_with(name: 'ConsultaForm:rfc')
+    rfc_field.value = rfc.to_s
+    button = check_form.button_with(name: 'ConsultaForm:_idJsp32')
+    result = agent.submit(check_form, button)
+    Nokogiri::HTML(result.body).search('.oBText').children
+  end
+  module_function :verify_rfc
 end
